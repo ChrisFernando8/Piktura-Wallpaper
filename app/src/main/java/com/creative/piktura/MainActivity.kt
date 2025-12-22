993556448021f67fe153987df6374335d7ed89e0
ChrisFernando8 <nandogkl2080@gmail.com>
2025-12-22 13:08:29 -0300

    Update MainActivity.kt

--- app/src/main/java/com/creative/piktura/MainActivity.kt
+++ app/src/main/java/com/creative/piktura/MainActivity.kt
@@ -11,25 +11,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val recycler = findViewById<RecyclerView>(R.id.recyclerView)
-        recycler.layoutManager = GridLayoutManager(this, 2)
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
 
-        RetrofitClient.api.getWallpapers().enqueue(object :
-            Callback<WallpaperResponse> {
+        // URLs do Cloudinary
+        val wallpapers = listOf(
+            "https://res.cloudinary.com/dbtturm2d/image/upload/v1766310175/wp1.jpg",
+            "https://res.cloudinary.com/dbtturm2d/image/upload/v1766310175/wp2.jpg",
+            "https://res.cloudinary.com/dbtturm2d/image/upload/v1766310175/wp3_p9d3xk.jpg"
+        )
 
-            override fun onResponse(
-                call: Call<WallpaperResponse>,
-                response: Response<WallpaperResponse>
-            ) {
-                val wallpapers = response.body()?.wallpapers ?: emptyList()
-                recycler.adapter = WallpaperAdapter(this@MainActivity, wallpapers)
-            }
-
-            override fun onFailure(call: Call<WallpaperResponse>, t: Throwable) {
-                Toast.makeText(this@MainActivity,
-                    "Erro ao carregar wallpapers",
-                    Toast.LENGTH_SHORT).show()
-            }
-        })
+        recyclerView.layoutManager = GridLayoutManager(this, 2)
+        recyclerView.adapter = WallpaperAdapter(this, wallpapers)
     }
 }
