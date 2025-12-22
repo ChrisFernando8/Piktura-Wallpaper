e895d2a5906859a493c9e6c1e0bc5f4cb97d3a0d
ChrisFernando8 <nandogkl2080@gmail.com>
2025-12-22 17:25:00 -0300

    Update WallpaperAdapter.kt

--- app/src/main/java/com/creative/piktura/WallpaperAdapter.kt
+++ app/src/main/java/com/creative/piktura/WallpaperAdapter.kt
@@ -1,1 +1,36 @@
+package com.creative.piktura
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+class WallpaperAdapter(
+    private val context: Context,
+    private val wallpapers: List<String>
+) : RecyclerView.Adapter<WallpaperAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context)
+            .inflate(R.layout.item_wallpaper, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val url = wallpapers[position]
+
+        Glide.with(context)
+            .load(url)
+            .centerCrop()
+            .into(holder.image)
+    }
+
+    override fun getItemCount(): Int = wallpapers.size
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val image: ImageView = itemView.findViewById(R.id.imgWallpaper)
+    }
+}  
