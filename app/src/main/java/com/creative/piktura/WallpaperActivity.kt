828ebc193df2194ad4441c05682320dda5ff1386
ChrisFernando8 <nandogkl2080@gmail.com>
2025-12-22 20:09:05 -0300

    Update WallpaperActivity.kt

--- app/src/main/java/com/creative/piktura/WallpaperActivity.kt
+++ app/src/main/java/com/creative/piktura/WallpaperActivity.kt
@@ -1,96 +1,25 @@
 package com.creative.piktura
 
-import android.app.WallpaperManager
-import android.graphics.Bitmap
-import android.graphics.drawable.Drawable
-import android.os.Build
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.bumptech.glide.Glide
-import com.bumptech.glide.request.target.CustomTarget
-import com.bumptech.glide.request.transition.Transition
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-class WallpaperActivity : AppCompatActivity() {
-
-    private lateinit var wallpaperImage: ImageView
-    private lateinit var imageUrl: String
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_wallpaper)
+        setContentView(R.layout.activity_main)
 
-        wallpaperImage = findViewById(R.id.wallpaperImage)
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
 
-        val btnHome = findViewById<Button>(R.id.btnHome)
-        val btnLock = findViewById<Button>(R.id.btnLock)
-        val btnBoth = findViewById<Button>(R.id.btnBoth)
+        val wallpapers = listOf(
+            "https://raw.githubusercontent.com/ChrisFernando8/Imagens-piktura/main/Wallpapers/wp1.jpg",
+            "https://raw.githubusercontent.com/ChrisFernando8/Imagens-piktura/main/Wallpapers/wp2.jpg",
+            "https://raw.githubusercontent.com/ChrisFernando8/Imagens-piktura/main/Wallpapers/wp3.jpg"
+        )
 
-        imageUrl = intent.getStringExtra("image_url") ?: ""
-
-        if (imageUrl.isEmpty()) {
-            Toast.makeText(this, "Imagem inválida", Toast.LENGTH_SHORT).show()
-            finish()
-            return
-        }
-
-        // 🔥 Preview FULLSCREEN
-        Glide.with(this)
-            .load(imageUrl)
-            .into(wallpaperImage)
-
-        btnHome.setOnClickListener {
-            applyWallpaper(WallpaperManager.FLAG_SYSTEM)
-        }
-
-        btnLock.setOnClickListener {
-            applyWallpaper(WallpaperManager.FLAG_LOCK)
-        }
-
-        btnBoth.setOnClickListener {
-            applyWallpaper(
-                WallpaperManager.FLAG_SYSTEM or WallpaperManager.FLAG_LOCK
-            )
-        }
-    }
-
-    private fun applyWallpaper(flag: Int) {
-        val manager = WallpaperManager.getInstance(this)
-
-        Glide.with(this)
-            .asBitmap()
-            .load(imageUrl)
-            .into(object : CustomTarget<Bitmap>() {
-
-                override fun onResourceReady(
-                    resource: Bitmap,
-                    transition: Transition<in Bitmap>?
-                ) {
-                    try {
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                            manager.setBitmap(resource, null, true, flag)
-                        } else {
-                            manager.setBitmap(resource)
-                        }
-
-                        Toast.makeText(
-                            this@WallpaperActivity,
-                            "Wallpaper aplicado!",
-                            Toast.LENGTH_SHORT
-                        ).show()
-
-                    } catch (e: Exception) {
-                        Toast.makeText(
-                            this@WallpaperActivity,
-                            "Erro ao aplicar wallpaper",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-                }
-
-                override fun onLoadCleared(placeholder: Drawable?) {}
-            })
+        recyclerView.layoutManager = GridLayoutManager(this, 2)
+        recyclerView.adapter = WallpaperAdapter(wallpapers)
     }
 }
