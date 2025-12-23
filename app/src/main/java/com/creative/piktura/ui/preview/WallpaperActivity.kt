9a417421e5654c95283ba1a73d6e9b07158cc061
ChrisFernando8 <nandogkl2080@gmail.com>
2025-12-23 00:48:13 -0300

    Update WallpaperActivity.kt

--- app/src/main/java/com/creative/piktura/ui/preview/WallpaperActivity.kt
+++ app/src/main/java/com/creative/piktura/ui/preview/WallpaperActivity.kt
@@ -1,1 +1,31 @@
+class WallpaperActivity : AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wallpaper)
+
+        val imageUrl = intent.getStringExtra("url") ?: return
+
+        Glide.with(this)
+            .asBitmap()
+            .load(imageUrl)
+            .into(object : CustomTarget<Bitmap>() {
+
+                override fun onResourceReady(
+                    resource: Bitmap,
+                    transition: Transition<in Bitmap>?
+                ) {
+                    findViewById<Button>(R.id.btnBoth).setOnClickListener {
+                        WallpaperApplier.apply(
+                            this@WallpaperActivity,
+                            resource,
+                            WallpaperManager.FLAG_SYSTEM or WallpaperManager.FLAG_LOCK
+                        )
+                        Toast.makeText(this@WallpaperActivity, "Wallpaper aplicado", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                override fun onLoadCleared(placeholder: Drawable?) {}
+            })
+    }
+}
