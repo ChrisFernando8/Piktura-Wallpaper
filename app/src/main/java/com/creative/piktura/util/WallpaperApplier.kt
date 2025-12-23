80a55f6508f6d439a1fa682196a4ca59b00e8a76
ChrisFernando8 <nandogkl2080@gmail.com>
2025-12-23 01:37:57 -0300

    Update WallpaperApplier.kt

--- app/src/main/java/com/creative/piktura/util/WallpaperApplier.kt
+++ app/src/main/java/com/creative/piktura/util/WallpaperApplier.kt
@@ -4,28 +4,16 @@ import android.app.WallpaperManager
 import android.content.Context
 import android.graphics.Bitmap
 import android.os.Build
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 
 object WallpaperApplier {
 
-    suspend fun applyFromUrl(
-        context: Context,
-        bitmap: Bitmap,
-        target: Int
-    ): Boolean = withContext(Dispatchers.IO) {
-        try {
-            val manager = WallpaperManager.getInstance(context)
+    fun apply(context: Context, bitmap: Bitmap, target: Int) {
+        val manager = WallpaperManager.getInstance(context)
 
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                manager.setBitmap(bitmap, null, true, target)
-            } else {
-                manager.setBitmap(bitmap)
-            }
-            true
-        } catch (e: Exception) {
-            e.printStackTrace()
-            false
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            manager.setBitmap(bitmap, null, true, target)
+        } else {
+            manager.setBitmap(bitmap)
         }
     }
 }
