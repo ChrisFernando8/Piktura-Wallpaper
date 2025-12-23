5a1c56141cd7abc4f22bbd8fcb441a0c93a0a54c
ChrisFernando8 <nandogkl2080@gmail.com>
2025-12-23 00:13:40 -0300

    Update WallpaperRepository.kt

--- app/src/main/java/com/creative/piktura/data/WallpaperRepository.kt
+++ app/src/main/java/com/creative/piktura/data/WallpaperRepository.kt
@@ -1,1 +1,12 @@
+package com.creative.piktura.data
 
+object WallpaperRepository {
+
+    fun getWallpapers(): List<String> {
+        return listOf(
+            "https://raw.githubusercontent.com/ChrisFernando8/Imagens-piktura/main/Wallpapers/wp1.jpg",
+            "https://raw.githubusercontent.com/ChrisFernando8/Imagens-piktura/main/Wallpapers/wp2.jpg",
+            "https://raw.githubusercontent.com/ChrisFernando8/Imagens-piktura/main/Wallpapers/wp3.jpg"
+        )
+    }
+}
