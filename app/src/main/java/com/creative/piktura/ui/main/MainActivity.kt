d7593e82660543fd4d308b94a238d03ddb9da93c
ChrisFernando8 <nandogkl2080@gmail.com>
2025-12-23 01:36:53 -0300

    Update MainActivity.kt

--- app/src/main/java/com/creative/piktura/ui/main/MainActivity.kt
+++ app/src/main/java/com/creative/piktura/ui/main/MainActivity.kt
@@ -1,1 +1,23 @@
+package com.creative.piktura.ui.main
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.creative.piktura.R
+import com.creative.piktura.data.WallpaperRepository
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val recycler = findViewById<RecyclerView>(R.id.recyclerView)
+
+        recycler.layoutManager = GridLayoutManager(this, 2)
+        recycler.adapter = WallpaperAdapter(
+            WallpaperRepository.getWallpapers()
+        )
+    }
+}
