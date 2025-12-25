9e159cca872fa9cb1728b343c1b4d8abc5264724
ChrisFernando8 <nandogkl2080@gmail.com>
2025-12-25 02:55:58 -0300

    Update MainViewModel.kt

--- app/src/main/java/com/creative/piktura/ui/main/MainViewModel.kt
+++ app/src/main/java/com/creative/piktura/ui/main/MainViewModel.kt
@@ -1,19 +1,11 @@
 package com.creative.piktura.ui.main
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.creative.piktura.data.model.Wallpaper
 import com.creative.piktura.data.repository.WallpaperRepository
 
 class MainViewModel : ViewModel() {
 
     private val repository = WallpaperRepository()
 
-    private val _wallpapers = MutableLiveData<List<Wallpaper>>()
-    val wallpapers: LiveData<List<Wallpaper>> = _wallpapers
-
-    fun loadWallpapers() {
-        _wallpapers.value = repository.getWallpapers()
-    }
+    val wallpapers = repository.getWallpapers()
 }
