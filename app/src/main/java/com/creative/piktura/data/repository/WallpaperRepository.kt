83ef341b028bf59c9bc27a5ba6316bd5cf5016c5
ChrisFernando8 <nandogkl2080@gmail.com>
2025-12-25 01:15:03 -0300

    Update WallpaperRepository.kt

--- app/src/main/java/com/creative/piktura/data/repository/WallpaperRepository.kt
+++ app/src/main/java/com/creative/piktura/data/repository/WallpaperRepository.kt
@@ -1,1 +1,29 @@
+package com.creative.piktura.data.repository
 
+import com.creative.piktura.data.model.Wallpaper
+import org.json.JSONObject
+import java.net.URL
+
+class WallpaperRepository {
+
+    private val DATA_URL =
+        "https://SEU_DOMINIO.pages.dev/wallpapers.json"
+
+    fun fetchWallpapers(): List<Wallpaper> {
+        val json = URL(DATA_URL).readText()
+        val array = JSONObject(json).getJSONArray("wallpapers")
+
+        val list = mutableListOf<Wallpaper>()
+        for (i in 0 until array.length()) {
+            val item = array.getJSONObject(i)
+            list.add(
+                Wallpaper(
+                    id = item.getInt("id"),
+                    title = item.getString("title"),
+                    image_url = item.getString("image_url")
+                )
+            )
+        }
+        return list
+    }
+}
